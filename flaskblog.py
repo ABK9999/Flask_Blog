468c147a3b3a340b149e1e064e3e46e80a8e6a01
ABK <Bhargav07491@gmail.com>
2023-01-06 03:15:59 +0530

    commit

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "<h1>Hello, World!</h1>"
+@app.route("/home")
+def home():
+    return "<h1>Home</h1>"
+
+@app.route("/")
+def about():
+    return "<h1>About</h1>"
 
 if __name__=='__main__':
     app.run(debug=True)
